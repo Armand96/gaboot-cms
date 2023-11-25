581e117eeedd69869947879bd4cf1ac7e7f40c0e
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-11-25 14:22:05 +0700

    add image

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
+import * as fs from 'fs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -8,5 +11,29 @@ export class AppController {
     @Get()
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Get('images')
+    images(@Req() req: Request, @Res() res: Response) {
+        if (req.query.image == undefined)
+        {
+            return res.status(404).json({
+                statusCode: 404,
+                error: 'Not Found',
+                message: 'Not Found',
+            });
+        }
+
+        const exist = fs.existsSync(join(process.cwd(), req.query.image as string));
+        if (exist) {
+            const file = fs.createReadStream(join(process.cwd(), req.query.image as string));
+            file.pipe(res);
+        } else {
+            return res.status(404).json({
+                statusCode: 404,
+                error: 'Not Found',
+                message: 'Not Found',
+            });
+        }
     }
 }
